889665457382c5a0f992dced0fe0ed343ad7ed89
ladibanks1 <oladipupobankole24@gmail.com>
2026-03-28 20:13:06 +0100

    fix: removed title tage from mail html text

--- server/src/mail/otp.service.ts
+++ server/src/mail/otp.service.ts
@@ -41,7 +41,6 @@ export class OtpService {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>EcomSphere OTP</title>
   <style>
     body {
       font-family: Arial, sans-serif;
